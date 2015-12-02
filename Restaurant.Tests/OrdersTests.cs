5229b506c5ffa5e6bb379632222916065ede6c78
maximillian2 <maxi.ua.1996@gmail.com>
2015-12-03 01:40:11 +0200

    Partially completed Order class with tests

---
+++ Restaurant.Tests/OrdersTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using Restaurant;
+
+namespace Restaurant.Tests
+{
+	[TestFixture ()]
+	public class OrdersTests
+	{
+		private Order order;
+
+		[SetUp]
+		public void BeforeRun()
+		{
+			order = new Order ();	
+		}
+
+		[Test]
+		public void DishAdded ()
+		{
+			var dish1 = new Dish ();
+			dish1.AddIngredient (new Ingredient ("Carrot", 10));
+			dish1.AddIngredient (new Ingredient ("Water", 1));
+			dish1.AddIngredient (new Ingredient ("Salt", 0.5));
+
+			order.AddDish (dish1);
+			Assert.Contains (dish1, order.Dishes);
+		}
+
+		[Test]
+		public void DishRemoved()
+		{
+			var dish1 = new Dish ();
+			dish1.AddIngredient (new Ingredient ("Carrot", 10));
+
+			order.AddDish (dish1);
+			order.RemoveDish (dish1);
+
+			Assert.That (order.Dishes.Count == 0);
+		}
+
+		[Test]
+		public void DishNameChanged()
+		{
+			var dish1 = new Dish ();
+			dish1.Name = "Borsch";
+			order.ChangeDishName (dish1, "Vareniki");
+
+			Assert.AreEqual (order.GetDishName(dish1), "Vareniki");
+		}
+
+		[Test]
+		public void DishPriceChanged()
+		{
+			var dish1 = new Dish ();
+
+			dish1.Price = 25.5;
+
+			order.ChangeDishPrice (dish1, 35.25);
+			Assert.AreEqual (order.GetDishPrice(dish1), 35.25);
+		}
+	}
+}
+
---
+++ Restaurant/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+	public class Order
+	{
+		public List<Dish> Dishes { get; set; }
+		// тз кривое, сумму написано сделать интовой, лол
+		public float TotalCost { get; set; }
+		public float TableNumber { get; set; }
+
+		public Order ()
+		{
+			TotalCost = 0;
+			TableNumber = 0;
+			Dishes = new List<Dish> ();
+		}
+
+		public void AddDish(Dish dish)
+		{
+			Dishes.Add (dish);
+		}
+
+		public void RemoveDish(Dish dish)
+		{
+			Dishes.Remove (dish);
+		}
+
+		public string GetDishName(Dish dish)
+		{
+			return dish.Name;
+		}
+
+		public void SetDishName(Dish dish, string name)
+		{
+			// make name validation here
+			// or override default setter
+			dish.Name = name;
+		}
+
+		public void ChangeDishName(Dish dish, string name)
+		{
+			SetDishName (dish, name);
+		}
+
+		public double GetDishPrice(Dish dish)
+		{
+			return dish.Price;
+		}
+
+		public void SetDishPrice(Dish dish, double price)
+		{
+			// some validation here
+			dish.Price = price;
+		}
+
+		public void ChangeDishPrice(Dish dish, double newPrice)
+		{
+			SetDishPrice (dish, newPrice);
+		}
+	}
+}
+

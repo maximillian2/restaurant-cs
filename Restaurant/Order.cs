dee826a45f0c988c758dbb0200c390eeda8b0a2c
maximillian2 <maxi.ua.1996@gmail.com>
2015-12-05 14:52:22 +0200

    Almost finish Order class

--- Restaurant/Order.cs
+++ Restaurant/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Restaurant
 {
@@ -17,47 +18,37 @@ namespace Restaurant
 			Dishes = new List<Dish> ();
 		}
 
-		public void AddDish(Dish dish)
-		{
-			Dishes.Add (dish);
-		}
+		public void AddDish(Dish dish) { Dishes.Add (dish); }
+		public void RemoveDish(Dish dish) { Dishes.Remove (dish); }
 
-		public void RemoveDish(Dish dish)
-		{
-			Dishes.Remove (dish);
-		}
-
-		public string GetDishName(Dish dish)
-		{
-			return dish.Name;
-		}
-
+		public string GetDishName(Dish dish) { return dish.Name; }
 		public void SetDishName(Dish dish, string name)
 		{
 			// make name validation here
 			// or override default setter
 			dish.Name = name;
 		}
+		public void ChangeDishName(Dish dish, string newName) { SetDishName (dish, newName); }
 
-		public void ChangeDishName(Dish dish, string name)
-		{
-			SetDishName (dish, name);
-		}
-
-		public double GetDishPrice(Dish dish)
-		{
-			return dish.Price;
-		}
-
+		public double GetDishPrice(Dish dish) { return dish.Price; }
 		public void SetDishPrice(Dish dish, double price)
 		{
 			// some validation here
 			dish.Price = price;
 		}
+		public void ChangeDishPrice(Dish dish, double newPrice) { SetDishPrice (dish, newPrice); }
 
-		public void ChangeDishPrice(Dish dish, double newPrice)
+		public double GetDishTime(Dish dish) { return dish.Time; }
+		public void SetDishTime(Dish dish, double newTime) 
+		{ 
+			// validation time!
+			dish.Time = newTime;
+		}
+		public void ChangeDishTime(Dish dish, double time) { SetDishTime (dish, time); }
+
+		public string GetDishInfo(Dish dish)
 		{
-			SetDishPrice (dish, newPrice);
+			return dish.ToString();
 		}
 	}
 }
